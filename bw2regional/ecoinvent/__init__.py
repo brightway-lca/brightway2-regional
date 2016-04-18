5b416fc664c49e98e83c89b4b706cf09cd80c3f6
Chris Mutel <cmutel@gmail.com>
2016-04-18 12:18:09 +0200

    Add ecoinvent handling

---
+++ bw2regional/ecoinvent/__init__.py
@@ -0,0 +1,11 @@
+from constructive_geometries import ConstructiveGeometries
+
+__all__ = (
+    "check_uuids",
+)
+
+
+from .names import check_uuids
+
+# self.data_fp = os.path.join(DATA_FILEPATH, u"faces.json")
+# self.faces_fp = os.path.join(DATA_FILEPATH, u"faces.gpkg")
