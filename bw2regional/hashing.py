e92a7e8536d81d9c8f498bc078aae1f73fadc4be
Chris Mutel <cmutel@gmail.com>
2016-04-19 11:15:49 +0200

    Add hashing.py

---
+++ bw2regional/hashing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from eight import *
+
+import hashlib
+
+
+def sha256(filepath, blocksize=65536):
+    """Generate SHA 256 hash for file at `filepath`"""
+    hasher = hashlib.sha256()
+    fo = open(filepath, 'rb')
+    buf = fo.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = fo.read(blocksize)
+    return hasher.hexdigest()
