c4532e08cad3ae8831c7f635db48c81c08eb60c2
Chris Mutel <cmutel@gmail.com>
2016-06-01 10:21:30 +0200

    Start refresh of graphics

---
+++ bw2regional/graphics/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    'RegionalizedGrapher',
+    'display_geocollection',
+)
+
+from .utils import normalize_array
+from .viridis_colormap import viridis
+from .image_output import display_geocollection
+from .spatial_output import RegionalizedGrapher
---
+++ bw2regional/graphics/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def normalize_array(array, nodata=None):
+    """Return ``array`` normalized to (0, 1).
+
+    Applies log transform is values span multiple orders of magnitude."""
+    # First, determine if log transform would help understand the data better
+    # Look at difference between
+    if nodata is not None:
+        mask = array != nodata
+    else:
+        mask = np.ones(array.shape)
+    copy = array[mask].ravel()
+    copy.sort()
+    top, bottom = copy[int(copy.shape[0] * 0.005)], copy[int(copy.shape[0] * 0.995)]
+    if top / bottom > 100 and copy.min() >= 0:
+        array[mask] = np.log(array[mask])
+
+    # Then normalize the values
+    # Set minimum value to 0
+    array[mask] = array[mask] - array[mask].min()
+    # Set maximum to 1
+    array[mask] = array[mask] / array[mask].max()
+    return array
