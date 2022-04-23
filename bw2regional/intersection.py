25d686e19a11f48861c5de3d23253a31961b0d04
Chris Mutel <cmutel@gmail.com>
2022-04-23 08:05:37 +0200

    Remove geopandas hack function

--- bw2regional/intersection.py
+++ bw2regional/intersection.py
@@ -1,11 +1,10 @@
 import copy
 import warnings
 
-import geopandas as gp
 from bw2data import geomapping
 from bw2data.ia_data_store import ImpactAssessmentDataStore
 
-from .meta import geocollections, intersections
+from .meta import intersections
 from .utils import create_certain_datapackage
 from .validate import intersection_validator
 
@@ -45,35 +44,3 @@ class Intersection(ImpactAssessmentDataStore):
             self,
             **extra_metadata
         )
-
-
-def write_intersection(first, second, overwrite=False):
-    """Calculate and write areal intersections between two vector geocollections"""
-    if (first, second) in intersections and not overwrite:
-        raise ValueError("Intersection already exists")
-
-    for gc in (first, second):
-        assert (
-            gc in geocollections
-            and geocollections[gc].get("kind") == "vector"
-            and "field" in geocollections[gc]
-        )
-    assert geocollections[first]["filepath"] != geocollections[second]["filepath"]
-
-    df1 = gp.read_file(geocollections[first]["filepath"])
-    df2 = gp.read_file(geocollections[second]["filepath"])
-    id1 = geocollections[first]["field"]
-    id2 = geocollections[second]["field"]
-
-    assert id1 != id2, "Conflicting ID labels"
-
-    intersection = gp.overlay(df1, df2)
-    areas = intersection.to_crs("esri:54009").area  # World Mollweidge in square meters
-
-    data = []
-    for index, feature in intersection.iterrows():
-        data.append(((first, feature[id1]), (second, feature[id2]), areas[index]))
-
-    obj = Intersection((first, second))
-    obj.write(data)
-    obj.create_reversed_intersection()
